68af589d854b7b9989b52fc86baef37a5352b503
nestorsb <nestorsecundario@gmail.com>
2022-02-18 01:20:43 +0100

    plantilla examen servidor 19/02/2022

---
+++ src/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\AbstractController;
+use App\Entity\Events;
+use App\Entity\Agent;
+use App\Entity\StatsEvents;
+use App\Core\EntityManager;
+use App\Entity\Uploads;
+use Doctrine\Common\Util\Debug;
+
+
+class MainController extends AbstractController
+{
+    public function main(){
+        
+    }
+}
--- src/Entity/Agent.php
+++ src/Entity/Agent.php
@@ -26,43 +26,15 @@ class Agent{
     private $agent_name;
 
     /**
-     * @ORM\Column(name="`password`", type="string", length=64)
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(name="faction", type="string", length=100)
-     */
-    private $faction;
-
-    /**
      * Un agente para muchos Uploads, Lado de Uno, bidireccional
      * @ORM\OneToMany(targetEntity="Uploads", mappedBy="agent")
      */
     private $uploads;
 
-    /**
-     * Un agente para muchas Estadísticas, Lado de Uno, bidireccional
-     * @ORM\OneToMany(targetEntity="Stats", mappedBy="agent")
-     */
-    private $stats;
-
-    /**
-     * Un agente para muchas Estadísticas de Eventos, Lado de Uno, bidireccional
-     * @ORM\OneToMany(targetEntity="StatsEvents", mappedBy="agent")
-     */
-    private $statsEvents;
-
-    /**
-     * Mediante el constructor se inicializan los ArrayCollection de las asociaciones de 
-     * uno a muchos que tenemos con la tabla agent
-     */
     public function __construct(){
-        //Inicializamos las variables asociadas como Arraycollection para poder
-        // obtener todos los objetos asociados
+
         $this->uploads = new ArrayCollection();
-        $this->stats = new ArrayCollection();
-        $this->statsEvents = new ArrayCollection();
+
     }
 
     /**
@@ -94,46 +66,6 @@ class Agent{
     }
 
     /**
-     * Get the value of password
-     */ 
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set the value of password
-     *
-     * @return  self
-     */ 
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of faction
-     */ 
-    public function getFaction()
-    {
-        return $this->faction;
-    }
-
-    /**
-     * Set the value of faction
-     *
-     * @return  self
-     */ 
-    public function setFaction($faction)
-    {
-        $this->faction = $faction;
-
-        return $this;
-    }
-
-    /**
      * Get de un agente para muchos Uploads, Lado de one mappedBy
      */ 
     public function getUploads()
@@ -152,54 +84,6 @@ class Agent{
         if(!$this->uploads->contains($uploads)){
             $this->uploads[] = $uploads;
             $uploads->setAgent($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get de un agente para muchas Estadísticas de Eventos, Lado de one Mapped
-     */ 
-    public function getStatsEvents()
-    {
-        return $this->statsEvents;
-    }
-
-    /**
-     * Set de un agente para muchas Estadísticas de Eventos, Lado de one Mapped
-     * donde también hemos realizado la insercción en la tabla StatsEvents el objeto Agente
-     *
-     * @return  self
-     */ 
-    public function setStatsEvents(StatsEvents $statsEvents)
-    {
-        if(!$this->statsEvents->contains($statsEvents)){
-            $this->statsEvents[] = $statsEvents;
-            $statsEvents->setAgent($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get un agente para muchas Estadísticas, Lado de one Mapped
-     */ 
-    public function getStats()
-    {
-        return $this->stats;
-    }
-
-    /**
-     * Set un agente para muchas Estadísticas, Lado de one Mapped
-     * donde también hemos realizado la insercción en la tabla Stats el objeto Agente
-     *
-     * @return  self
-     */ 
-    public function setStats(Stats $stats)
-    {
-        if(!$this->stats->contains($stats)){
-            $this->stats[] = $stats;
-            $stats->setAgent($this);
         }
 
         return $this;
